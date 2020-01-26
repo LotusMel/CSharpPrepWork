3abaa89ffc2a0ad0530b307dc1071fa7275c8d78
Melva Lang <mjlang74@gmail.com>
2020-01-26 01:51:26 -0600

    Added Hello controller.

---
+++ HelloMVC2020/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC2020.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("Hello World");
+        }
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+}
