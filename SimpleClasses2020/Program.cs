bc616316e8c3a04c293cd89eff3efd6a4c4dfb3d
Melva Lang <mjlang74@gmail.com>
2020-03-06 12:20:21 -0600

    Classes video

---
+++ SimpleClasses2020/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleClasses2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I own a car lot. I want to build an app to 
+            //keep track of the cars on my lot.
+            //So I create variables.
+
+            //I could do this for each car:
+
+            //string car1Make;
+            //string car1Model;
+            //int car1Year;
+            //string car1Color;
+
+            //string car2Make;
+            //string car2Model;
+            //int car2Year;
+
+            //string car3Make;
+            //string car3Model;
+            //int car3Year;
+
+            //...but this gets lengthy.
+
+        }
+    }
+}
