5a4b52d67dadc8c562d77be2c3b80c6865adce05
Melva Lang <mjlang74@gmail.com>
2020-03-11 20:18:33 -0500

    Cheese model

--- CheeseMVC2020/Controllers/CheeseController.cs
+++ CheeseMVC2020/Controllers/CheeseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CheeseMVC2020.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,7 +11,7 @@ namespace CheeseMVC2020.Controllers
 {
     public class CheeseController : Controller
     {
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        static private List<Cheese> Cheeses = new List<Cheese>();
 
         // GET: /<controller>/
         public IActionResult Index()
@@ -25,31 +26,49 @@ namespace CheeseMVC2020.Controllers
             return View();
         }
 
-        public IActionResult Remove()
-        {
-            ViewBag.cheeses = Cheeses;
-            return View();
-        }
+        //public IActionResult Remove()
+        //{
+        //    ViewBag.cheeses = Cheeses;
+        //    return View();
+        //}
 
         [HttpPost]
         [Route("/Cheese/Add")]
         public IActionResult NewCheese(string name, string describe)
         {
+            Cheese newCheese = new Cheese
+            {
+                Description = describe,
+                Name = name
+            };
+
             // Add the new cheese to my existing cheeses
-            Cheeses.Add(name, describe);
+            Cheeses.Add(newCheese);
+
             return Redirect("/Cheese");
         }
 
-        [HttpPost]
-        [Route("/Cheese/Remove")]
-        public IActionResult RemoveCheese(string name)
-        {
+        //[HttpPost]
+        //[Route("/Cheese/Remove")]
+        //public IActionResult RemoveCheese(string name)
+        //{
             
-            Cheeses.Remove(name);
+        //    Cheeses.Remove(name);
 
-            return Redirect("/Cheese");
+        //    return Redirect("/Cheese");
 
-        }
+        //}
+
+        //public IActionResult Edit(int cheeseId)
+        //{
+
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(int cheeseId, string name, string description)
+        //{
+
+        //}
 
     }
 }
